8c7ca2ddfc8db0c588507ec89c19e35f32437198
Gary <75078672stevenmuriel@gmail.com>
2023-11-01 00:00:27 -0500

    Plantilla base para el proyecto frontend de facturacion

--- Frontend/.eslintrc.cjs
+++ Frontend/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, "node": true },
+  env: { browser: true, es2020: true, 'node': true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -16,7 +16,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'indent': ["error", 2],
-    "no-console": "off",
+    'indent': ['error', 2],
+    'no-console': 'off',
+    'quotes': ['error', 'single'],
   },
 }
